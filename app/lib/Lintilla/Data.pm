42e075c5433731a66fec3bfab25d2a013dbe5944
Andy Armstrong <andy@hexten.net>
2013-12-04 16:55:39 +0000

    Page through images.

--- app/lib/Lintilla/Data.pm
+++ app/lib/Lintilla/Data.pm
@@ -11,6 +11,8 @@ Lintilla::Data - Data handlers
 
 =cut
 
+use constant MAX_PAGE => 500;
+
 my %REF = map { $_ => 1 } qw(
  collection copyright_class copyright_holder format kind location
  news_restriction personality photographer subject
@@ -22,9 +24,22 @@ sub refdata {
   database->selectall_hashref( "SELECT id, name FROM elvis_$name", 'id' );
 }
 
+sub page {
+  my ( $start, $size ) = @_;
+  $size = MAX_PAGE if $size > MAX_PAGE;
+  database->selectall_arrayref(
+    "SELECT * FROM elvis_image ORDER BY acno ASC LIMIT ?, ?",
+    { Slice => {} },
+    $start, $size
+  );
+}
+
 prefix '/data' => sub {
   get '/ref/:name' => sub {
     return refdata( param('name') );
+  };
+  get '/page/:size/:start' => sub {
+    return page( param('start'), param('size') );
   };
 };
 
