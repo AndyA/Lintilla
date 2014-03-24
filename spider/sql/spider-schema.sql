6c3d4c092698a4386f2d20b4a5c4142194b5e3de
Andy Armstrong <andy@hexten.net>
2014-03-24 16:40:55 +0000

    Spider.

---
+++ spider/sql/spider-schema.sql
@@ -0,0 +1,76 @@
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `spider`;
+USE `spider`;
+-- MySQL dump 10.15  Distrib 10.0.9-MariaDB, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: spider
+-- ------------------------------------------------------
+-- Server version	10.0.9-MariaDB-1~wheezy
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `spider_page`
+--
+
+DROP TABLE IF EXISTS `spider_page`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `spider_page` (
+  `url` text NOT NULL,
+  `url_hash` varchar(64) NOT NULL,
+  `mime` varchar(255) DEFAULT NULL,
+  `rank` int(10) DEFAULT NULL,
+  `body` longtext,
+  `header` mediumtext,
+  `code` int(3) DEFAULT NULL,
+  `message` text,
+  `last_visit` int(12) unsigned DEFAULT NULL,
+  `worker_start` int(12) unsigned DEFAULT NULL,
+  `worker_id` varchar(80) DEFAULT NULL,
+  PRIMARY KEY (`url_hash`),
+  KEY `worker_id` (`worker_id`),
+  KEY `worker_start` (`worker_start`),
+  KEY `last_visit` (`last_visit`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `spider_via`
+--
+
+DROP TABLE IF EXISTS `spider_via`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `spider_via` (
+  `url_hash` varchar(64) NOT NULL,
+  `via_hash` varchar(64) NOT NULL,
+  `last_visit` int(12) unsigned DEFAULT NULL,
+  UNIQUE KEY `hash` (`url_hash`,`via_hash`),
+  KEY `via_hash` (`via_hash`),
+  KEY `url_hash` (`url_hash`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping routines for database 'spider'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2014-03-24 15:52:20
---
+++ spider/tools/spider.pl
@@ -0,0 +1,371 @@
+#!/usr/bin/env perl
+
+use v5.10;
+
+use strict;
+use warnings;
+
+use DBI;
+use Digest::MD5 qw( md5_hex );
+use Getopt::Long;
+use HTML::LinkExtor;
+use JSON;
+use LWP::UserAgent;
+use Sys::Hostname;
+use URI;
+
+use constant HOST  => 'localhost';
+use constant USER  => 'root';
+use constant PASS  => '';
+use constant DB    => 'spider';
+use constant PROXY => 'http://andy:froonbat1127@spider.hexten.net:3128/';
+
+my @ROOT = qw(
+ http://explore.gateway.bbc.co.uk/ResearchGateway/researchgateway/music1.aspx
+ http://explore.gateway.bbc.co.uk/ResearchGateway/researchgateway/tv_and_radio1.aspx
+ http://explore.gateway.bbc.co.uk/ResearchGateway/researchgateway/stills_and_photos.aspx
+ http://explore.gateway.bbc.co.uk/ResearchGateway/research_gateway_2012/reference.aspx
+ http://explore.gateway.bbc.co.uk/ResearchGateway/researchgateway/people.aspx
+ http://www.audiencesportal.com/default.aspx
+ http://bbcapps2498.national.core.bbc.co.uk/home.aspx
+ http://fgbw1wsrot1202.national.core.bbc.co.uk/autorot/
+);
+
+$| = 1;
+
+GetOptions() or die;
+
+my $verb = shift // 'help';
+
+if ( $verb eq 'init' ) {
+  my $dbh = dbh(DB);
+  for my $root (@ROOT) {
+    schedule( $dbh, $root, 0 );
+  }
+  $dbh->disconnect;
+}
+elsif ( $verb eq 'spider' ) {
+  my $dbh = dbh(DB);
+  reap($dbh);
+  spider($dbh);
+  $dbh->disconnect;
+}
+elsif ( $verb eq 'help' ) {
+  print "spider.pl init|spider|help\n";
+}
+else {
+  die "Unknown command: $verb\n";
+}
+
+sub claim_url {
+  my ( $dbh, $sel, $ord ) = @_;
+  update(
+    $dbh,
+    'spider_page',
+    { worker_id    => worker_id(),
+      worker_start => time,
+    },
+    { LIMIT => 1, %$sel },
+    $ord
+  );
+}
+
+sub got_url {
+  my $dbh = shift;
+  return @{
+    $dbh->selectall_arrayref(
+      "SELECT * FROM spider_page WHERE worker_id=?",
+      { Slice => {} },
+      worker_id()
+    ) };
+}
+
+sub start_work {
+  my $dbh = shift;
+  my @work;
+  return @work if @work = got_url($dbh);
+  claim_url( $dbh, { worker_id => undef, last_visit => undef } );
+  return @work if @work = got_url($dbh);
+  claim_url( $dbh, { worker_id => undef }, ['last_visit'] );
+  return got_url($dbh);
+}
+
+sub fill_in {
+  my $rec = shift;
+  return { %{$rec}, url_hash => md5_hex( $rec->{url} ) };
+}
+
+sub end_work {
+  my ( $dbh, $rec ) = @_;
+  my $frec = fill_in( { %$rec, last_visit => time, worker_id => undef } );
+  update( $dbh, 'spider_page', $frec, { url_hash => $frec->{url_hash} } );
+}
+
+sub spider {
+  my $dbh   = shift;
+  my @queue = @_;
+
+  my $ua   = LWP::UserAgent->new;
+  my $json = JSON->new->canonical->utf8;
+  $ua->proxy( ['http', 'https'], PROXY );
+
+  while ( my @work = start_work($dbh) ) {
+    for my $job (@work) {
+      my $url = URI->new( $job->{url} );
+      unless ( should_visit($url) ) {
+        print "Skipping $url\n";
+        end_work(
+          $dbh,
+          { url     => $job->{url},
+            message => 'Skipped by URL based rule',
+            code    => 0,
+          }
+        );
+        next;
+      }
+
+      print "Fetching $url\n";
+      my $resp = $ua->get($url);
+
+      my $got = {
+        url     => $job->{url},
+        code    => $resp->code,
+        message => $resp->status_line,
+        header  => $json->encode( headers($resp) ),
+        body    => $resp->content,
+        mime    => scalar( $resp->content_type ),
+      };
+
+      if ( $got->{mime} eq 'text/html' ) {
+        for my $link ( find_links($resp) ) {
+          next unless should_visit($link);
+          via( $dbh, $link, $job->{url} );
+          schedule( $dbh, $link, $job->{rank} + 1 );
+        }
+      }
+
+      end_work( $dbh, $got );
+
+      print sprintf "%s\n", $resp->status_line;
+    }
+  }
+}
+
+sub clean_url {
+  my $url = shift;
+  $url->fragment(undef);
+  return $url;
+}
+
+sub find_links {
+  my $resp = shift;
+
+  my @links = ();
+
+  HTML::LinkExtor->new(
+    sub {
+      my ( $tag, %attr ) = @_;
+      return if $tag ne 'a';
+      my $href = $attr{href};
+      push @links, $href
+       unless $href =~ /^\w+:/ && $href !~ /^https?:/;
+    }
+  )->parse( $resp->content );
+
+  my $base = $resp->base;
+  return map { clean_url( URI->new_abs( $_, $base ) ) } @links;
+}
+
+sub headers {
+  my $resp = shift;
+  return { map { $_ => [$resp->header($_)] } $resp->header_field_names };
+}
+
+sub worker_id { join '.', hostname, $$ }
+
+sub should_visit {
+  my $url = shift;
+  return unless $url->host =~ /\.bbc\.co\.uk$/;
+  return if $url->host eq 'www.bbc.co.uk';
+  return if $url->host eq 'ssl.bbc.co.uk';
+  return if $url->host eq 'iplayerhelp.external.bbc.co.uk';
+  return if $url->host =~ /\bbetsie\b/;
+  return 1;
+}
+
+sub reap {
+  my $dbh     = shift;
+  my $hn      = hostname;
+  my @workers = @{
+    $dbh->selectcol_arrayref(
+      'SELECT DISTINCT(worker_id) FROM spider_page WHERE worker_id IS NOT NULL'
+    ) };
+  for my $worker (@workers) {
+    next unless $worker =~ /^\Q$hn.\E(\d+)$/;
+    my $pid = $1;
+    next if kill 0, $pid;
+    print "Reaping jobs for $worker\n";
+    update(
+      $dbh, 'spider_page',
+      { worker_start => undef, worker_id => undef },
+      { worker_id    => $worker }
+    );
+  }
+}
+
+sub via {
+  my ( $dbh, $url, $via ) = @_;
+  my $now = time;
+  my $sql
+   = 'INSERT INTO `spider_via` (`url_hash`, `via_hash`, `last_visit`) '
+   . 'VALUES (?, ?, ?) '
+   . 'ON DUPLICATE KEY UPDATE `last_visit` = ?';
+  $dbh->prepare($sql)->execute( md5_hex($url), md5_hex($via), $now, $now );
+}
+
+sub schedule {
+  my ( $dbh, $url, $rank ) = @_;
+
+  my $sql
+   = 'INSERT INTO `spider_page` (`url`, `url_hash`, `rank`, `last_visit`) '
+   . 'VALUES (?, ?, ?, ?) '
+   . 'ON DUPLICATE KEY UPDATE `rank`=IF(`rank` > ?, ?, `rank`)';
+
+  $dbh->prepare($sql)
+   ->execute( $url, md5_hex($url), $rank, 0, $rank, $rank );
+}
+
+sub trim {
+  my $s = shift;
+  s/^\s+//, s/\s+$// for $s;
+  return $s;
+}
+
+sub transaction {
+  my ( $dbh, $cb ) = @_;
+  $dbh->do('START TRANSACTION');
+  eval { $cb->() };
+  if ( my $err = $@ ) {
+    $dbh->do('ROLLBACK');
+    die $err;
+  }
+  $dbh->do('COMMIT');
+}
+
+sub insert {
+  my ( $dbh, $tbl, $rec ) = @_;
+  my @k = sort keys %$rec;
+  my $sql
+   = "INSERT INTO `$tbl` ("
+   . join( ', ', map "`$_`", @k )
+   . ") VALUES ("
+   . join( ', ', map '?', @k ) . ")";
+  my $sth = $dbh->prepare($sql);
+  $sth->execute( @{$rec}{@k} );
+}
+
+sub make_where {
+  my $sel = shift;
+  my ( @bind, @term );
+  for my $k ( sort keys %$sel ) {
+    my $v = $sel->{$k};
+    my ( $op, $vv )
+     = 'ARRAY' eq ref $v ? @$v : ( ( defined $v ? '=' : 'IS' ), $v );
+    if ( 'ARRAY' eq ref $vv ) {
+      push @term, "`$k` $op (" . join( ', ', map '?', @$vv ) . ")";
+      push @bind, @$vv;
+    }
+    else {
+      push @term, "`$k` $op ?";
+      push @bind, $vv;
+    }
+  }
+  @term = ('TRUE') unless @term;
+  return ( join( ' AND ', @term ), @bind );
+}
+
+sub update {
+  my ( $dbh, $tbl, $rec, $sel, $ord ) = @_;
+
+  my ( @fld, @bind );
+  while ( my ( $k, $v ) = each %$rec ) {
+    push @fld,  $k;
+    push @bind, $v;
+  }
+
+  my $limit = delete $sel->{LIMIT};
+  my ( $where, @wbind ) = make_where($sel);
+
+  my $sql
+   = "UPDATE `$tbl` SET "
+   . join( ', ', map "`$_`=?", @fld )
+   . " WHERE $where";
+
+  $sql .= ' ORDER BY ' . join ', ',
+   map { /^-(.+)/ ? "`$1` DESC" : "`$_` ASC" } @$ord
+   if $ord;
+
+  $sql .= " LIMIT $limit" if defined $limit;
+
+  $dbh->prepare($sql)->execute( @bind, @wbind );
+}
+
+sub execute_select {
+  my ( $dbh, $tbl, $sel, $cols, $ord ) = @_;
+
+  my ( $sql, @bind ) = make_select( $tbl, $sel, $cols, $ord );
+  #  print show_sql( $sql, @bind ), "\n";
+  my $sth = $dbh->prepare($sql);
+  $sth->execute(@bind);
+  return $sth;
+}
+
+sub make_select {
+  my ( $tbl, $sel, $cols, $ord ) = @_;
+
+  my $limit = delete $sel->{LIMIT};
+  my ( $where, @bind ) = make_where($sel);
+
+  my @sql = (
+    'SELECT',
+    ( $cols ? join ', ', map "`$_`", @$cols : '*' ),
+    "FROM `$tbl` WHERE ", $where
+  );
+
+  if ($ord) {
+    push @sql, "ORDER BY", join ', ',
+     map { /^-(.+)/ ? "`$1` DESC" : "`$_` ASC" } @$ord;
+  }
+
+  if ( defined $limit ) {
+    push @sql,  'LIMIT ?';
+    push @bind, $limit;
+  }
+
+  return ( join( ' ', @sql ), @bind );
+}
+
+sub show_sql {
+  my ( $sql, @bind ) = @_;
+  my $next = sub {
+    my $val = shift @bind;
+    return 'NULL' unless defined $val;
+    return $val if $val =~ /^\d+(?:\.\d+)?$/;
+    $val =~ s/\\/\\\\/g;
+    $val =~ s/\n/\\n/g;
+    $val =~ s/\t/\\t/g;
+    return "'$val'";
+  };
+  $sql =~ s/\?/$next->()/eg;
+  return $sql;
+}
+
+sub dbh {
+  my $db = shift;
+  return DBI->connect(
+    sprintf( 'DBI:mysql:database=%s;host=%s', $db, HOST ),
+    USER, PASS, { RaiseError => 1 } );
+}
+
+# vim:ts=2:sw=2:sts=2:et:ft=perl
+
