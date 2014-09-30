12f5adf9e409863d9329a06a71d22d1b552cfa26
Andy Armstrong <andy@hexten.net>
2014-09-30 21:18:59 +0100

    More sync WIP. Refs #87

--- Sync/Agent.pm
+++ Sync/Agent.pm
@@ -13,13 +13,8 @@ Lintilla::Sync::Agent - Sync agent
 
 has sync_base => ( is => 'ro', required => 1, isa => 'Str' );
 has ['sync_user', 'sync_pass'] => ( is => 'ro', isa => 'Maybe[Str]' );
-
-has _ua => (
-  is      => 'ro',
-  isa     => 'LWP::UserAgent',
-  lazy    => 1,
-  builder => '_b_ua',
-);
+has _ua   => ( is => 'ro', lazy => 1, builder => '_b_ua', );
+has _json => ( is => 'ro', lazy => 1, builder => '_b_json', );
 
 sub _netloc {
   my $self = shift;
@@ -27,15 +22,27 @@ sub _netloc {
   return join ':', $u->host, $u->port;
 }
 
+sub _endpoint {
+  my ( $self, @part ) = @_;
+  ( my $base = $self->sync_base ) =~ s@/$@@;
+  return join '/', $base, @part;
+}
+
 sub _b_ua {
   my $self = shift;
   my $ua   = LWP::UserAgent->new;
-  $ua->credentials( $self->_netloc, 'Genome Sync', $self->sync_user,
+  $ua->credentials( $self->_netloc, 'RT Infax Bridge', $self->sync_user,
     $self->sync_pass );
+  return $ua;
 }
+
+sub _b_json { JSON->new->utf8 }
 
 sub get_changes {
   my ( $self, $since ) = @_;
+  my $resp = $self->_ua->get( $self->_endpoint( changes => $since ) );
+  die $resp->status_line if $resp->is_error;
+  return $self->_json->decode( $resp->content );
 }
 
 1;
