bbec7fd13ceaeac462529546b73e8d01a21ed850
Andy Armstrong <andy@hexten.net>
2015-02-16 12:30:04 +0000

    Dependency based resource loading. Refs #357

--- Site/Admin.pm
+++ Site/Admin.pm
@@ -7,6 +7,7 @@ use JSON qw();
 use Lintilla::Broadcast::Client;
 use Lintilla::Broadcast::Server;
 use Lintilla::DB::Genome::Edit;
+use Lintilla::Tools::Enqueue;
 use Time::HiRes qw( time );
 
 =head1 NAME
@@ -19,6 +20,94 @@ our $VERSION = '0.1';
 
 my $CLIENT = Lintilla::Broadcast::Client->new;
 my $SERVER = Lintilla::Broadcast::Server->new->listen;
+
+my $ENQUEUE = Lintilla::Tools::Enqueue->new(
+  map => {
+    css => {
+      admin2 => { url => '/admin2/css/admin2.css' },
+      jquery_labeledslider =>
+       { url => '/admin2/css/jquery.ui.labeledslider.css' },
+      fa        => { url => '/css/font-awesome/css/font-awesome.min.css' },
+      jquery_ui => { url => '/css/jquery-ui.min.css' },
+    },
+    js => {
+      uri       => { url => '/admin2/js/URI.js' },
+      spin      => { url => '/admin2/js/spin.min.js' },
+      jquery    => { url => '/js/jquery-1.11.1.min.js' },
+      diff_lcs  => { url => '/admin2/js/diff-lcs.js' },
+      jquery_ui => {
+        url      => '/js/jquery-ui.min.js',
+        requires => ['css.jquery_ui', 'js.jquery']
+      },
+      jquery_hotkeys => {
+        url      => '/admin2/js/jquery.hotkeys.js',
+        requires => ['js.jquery']
+      },
+      jquery_labeledslider => {
+        url      => '/admin2/js/jquery.ui.labeledslider.min.js',
+        requires => ['css.jquery_labeledslider', 'js.jquery_ui']
+      },
+      util => {
+        url      => '/admin2/js/util.js',
+        requires => ['js.jquery']
+      },
+      scroller => {
+        url      => '/admin2/js/scroller.js',
+        requires => ['js.jquery']
+      },
+      progress => {
+        url      => '/admin2/js/progress.js',
+        requires => ['js.spin', 'js.jquery']
+      },
+      datawatcher => {
+        url      => '/admin2/js/datawatcher.js',
+        requires => ['js.jquery', 'js.ev']
+      },
+      ev => {
+        url      => '/admin2/js/ev.js',
+        requires => ['js.jquery']
+      },
+      htmldiff => {
+        url      => '/admin2/js/htmldiff.js',
+        requires => ['js.jquery']
+      },
+      programme => {
+        url      => '/admin2/js/programme.js',
+        requires => ['js.jquery']
+      },
+      versions => {
+        url => '/admin2/js/versions.js',
+        requires =>
+         ['js.jquery', 'js.jquery_labeledslider', 'js.textdiff', 'js.htmldiff']
+      },
+      textdiff => {
+        url      => '/admin2/js/textdiff.js',
+        requires => ['js.diff_lcs']
+      },
+      app => {
+        url      => '/admin2/js/app.js',
+        requires => ['js.jquery']
+      },
+      adminapp => {
+        url      => '/admin2/js/adminapp.js',
+        requires => ['js.jquery', 'js.datawatcher', 'js.app']
+      },
+      wysihtml5_parser_rules =>
+       { url => '/admin2/js/wysihtml5/parser_rules/advanced.js' },
+      wysihtml5 => {
+        url      => '/admin2/js/wysihtml5/wysihtml5-0.3.0.min.js',
+        requires => ['js.wysihtml5_parser_rules']
+      },
+      approve => {
+        url      => '/admin2/js/approve.js',
+        requires => [
+          'css.admin2',  'js.jquery', 'js.adminapp', 'js.programme',
+          'js.versions', 'js.util',   'js.progress', 'js.uri',
+          'js.scroller', 'js.jquery_hotkeys',
+        ]
+      },
+    } }
+);
 
 sub db() {
   my $db = Lintilla::DB::Genome::Edit->new( dbh => database );
@@ -153,9 +242,8 @@ prefix '/admin2' => sub {
     'internal',
     sub {
       template 'admin2/approve',
-       {title       => 'Genome Admin',
-        scripts     => ['approve'],
-        stylesheets => [],
+       {title     => 'Genome Admin',
+        resources => $ENQUEUE->render('js.approve'),
        },
        { layout => 'admin2' };
     }
---
+++ Tools/Enqueue.pm
@@ -0,0 +1,103 @@
+package Lintilla::Tools::Enqueue;
+
+use Moose;
+
+use HTML::Tiny;
+
+=head1 NAME
+
+Lintilla::Tools::Enqueue - Enqueue JS/CSs
+
+=cut
+
+has map => ( isa => 'HashRef', is => 'ro', required => 1 );
+
+has _deps => (
+  traits  => ['Array'],
+  isa     => 'ArrayRef',
+  is      => 'ro',
+  default => sub { [] },
+  handles => { enqueue => 'push' }
+);
+
+has _render => (
+  isa     => 'HashRef[Code]',
+  is      => 'ro',
+  default => sub { {} },
+);
+
+sub BUILD {
+  my $self = shift;
+  my $h    = HTML::Tiny->new;
+  $self->formatter(
+    css => sub {
+      $h->link( { href => $_[0]{url}, rel => 'stylesheet' } );
+    },
+    js => sub {
+      $h->script( { src => $_[0]{url}, type => 'text/javascript' } );
+    }
+  );
+}
+
+sub formatter {
+  my ( $self, %h ) = @_;
+  my $r = $self->_render;
+  while ( my ( $type, $code ) = each %h ) {
+    $r->{$type} = $code;
+  }
+}
+
+sub _enqueue {
+  my ( $self, $type, @obj ) = @_;
+  return $self->enqueue( map { "$type.$_" } @obj );
+}
+
+sub js {
+  my ( $self, @js ) = @_;
+  return $self->_enqueue( js => @js );
+}
+
+sub css {
+  my ( $self, @css ) = @_;
+  return $self->_enqueue( css => @css );
+}
+
+sub _resolve {
+  my ( $self, $name ) = @_;
+  my ( $type, $tag ) = split /\./, $name, 2;
+  return ( $self->map->{$type}{$tag}, $type, $tag ) if wantarray;
+  return $self->map->{$type}{$tag};
+}
+
+sub _expand {
+  my ( $self, $seen, @obj ) = @_;
+  my @list = ();
+  for my $obj (@obj) {
+    next if $seen->{$obj}++;
+    my $rr = $self->_resolve($obj);
+    push @list, $self->_expand( $seen, @{ $rr->{requires} // [] } ), $obj;
+  }
+  return @list;
+}
+
+sub expand {
+  my $self = shift;
+  return [$self->_expand( {}, @{ $self->_deps }, @_ )];
+}
+
+sub render {
+  my $self = shift;
+  my $dep  = $self->expand(@_);
+  my $rm   = $self->_render;
+  my @out  = ();
+  for my $obj (@$dep) {
+    my ( $rr, $type, $tag ) = $self->_resolve($obj);
+    my $rc = $rm->{$type} // die "No renderer for $type";
+    push @out, $rc->($rr);
+  }
+  return join "\n", @out, '';
+}
+
+1;
+
+# vim:ts=2:sw=2:sts=2:et:ft=perl
