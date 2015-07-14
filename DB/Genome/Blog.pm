6f1a8f0ecebc559b6245cceebb2c06c3f4a0a5de
Andy Armstrong <andy@hexten.net>
2015-07-14 21:37:10 +0100

    Find programme links in blog.

--- DB/Genome/Blog.pm
+++ DB/Genome/Blog.pm
@@ -5,8 +5,10 @@ use v5.10;
 use Moose;
 
 use DateTime::Format::Mail;
+use HTML::LinkExtor;
 use LWP::UserAgent;
 use Text::HTMLCleaner;
+use URI;
 use XML::LibXML::XPathContext;
 use XML::LibXML;
 
@@ -16,8 +18,7 @@ with 'Lintilla::Role::DB';
 with 'Lintilla::Role::JSON';
 with 'Lintilla::Role::Config';
 with 'Lintilla::Role::DateTime';
-
-use constant PURGE => 0;
+with 'Lintilla::Role::UUID';
 
 =head1 NAME
 
@@ -83,12 +84,85 @@ sub _decode_rss {
   return @items;
 }
 
-sub _extract_text {
-  my ( $self, @items ) = @_;
-  for my $item (@items) {
-    next unless defined $item->{content};
+sub _find_links {
+  my ( $self, $html ) = @_;
+  my $ex = HTML::LinkExtor->new;
+  $ex->parse($html);
+  $ex->eof;
+  my @out = ();
+  for my $set ( $ex->links ) {
+    my ( $tag, %links ) = @$set;
+    push @out, values %links;
+  }
+  return @out;
+}
+
+sub _find_genome_links {
+  my ( $self, @links ) = @_;
+
+  # Fix broken links
+  s|^(genome\.ch\.bbc\.co\.uk)|http://$1| for @links;
+
+  my %seen = ();
+  my @out  = ();
+  for my $link (@links) {
+    my $uri  = URI->new($link);
+    my $path = $uri->path;
+    next if $seen{$path}++;
+    my @part = split /\//, $path;
+    next
+     unless @part == 2
+     && $part[0] eq ""
+     && $part[1] =~ /^[0-9a-f]{32}$/i;
+    push @out, $self->format_uuid( $part[1] );
+  }
+  return @out;
+}
+
+sub _update_item {
+  my ( $self, $item ) = @_;
+
+  my @links = ();
+  if ( defined $item->{content} ) {
     my $tc = Text::HTMLCleaner->new( html => $item->{content} );
     $item->{full_text} = $tc->text;
+    push @links,
+     $self->_find_genome_links( $self->_find_links( $item->{content} ) );
+  }
+
+  my ($id)
+   = $self->dbh->selectrow_array(
+    "SELECT `id` FROM `genome_blog_feed` WHERE `guid` = ?",
+    {}, $item->{guid} );
+
+  $item->{id} = $id if defined $id;
+
+  my @cols = sort keys %$item;
+
+  $self->dbh->do(
+    join( " ",
+      "REPLACE INTO `genome_blog_feed` (",
+      join( ", ", map "`$_`", @cols ),
+      ") VALUES (",
+      join( ", ", map "?", @cols ),
+      ")" ),
+    {},
+    @{$item}{@cols}
+  );
+
+  $id = $self->dbh->last_insert_id( undef, undef, undef, undef )
+   unless defined $id;
+
+  $self->dbh->do( "DELETE FROM `genome_blog_link` WHERE `blog_id` = ?",
+    {}, $id );
+  if (@links) {
+    $self->dbh->do(
+      join( " ",
+        "INSERT INTO `genome_blog_link` (`blog_id`, `programme`) VALUES",
+        join( ", ", map "(?, ?)", @links ) ),
+      {},
+      map { $id, $_ } @links
+    );
   }
 }
 
@@ -97,7 +171,6 @@ sub _update_blog {
 
   my $rss   = $self->_fetch($uri);
   my @items = $self->_decode_rss($rss);
-  $self->_extract_text(@items);
 
   return unless @items;
 
@@ -110,22 +183,9 @@ sub _update_blog {
       $version //= 1;
 
       for my $item (@items) {
-        my @cols = sort keys %$item;
-        $self->dbh->do(
-          join( " ",
-            "REPLACE INTO `genome_blog_feed` (",
-            join( ", ", map "`$_`", "blog", "version", @cols ),
-            ") VALUES (",
-            join( ", ", map "?", "blog", "version", @cols ),
-            ")" ),
-          {},
-          $name, $version,
-          @{$item}{@cols}
-        );
-      }
-      if (PURGE) {
-        $self->dbh->do( "DELETE FROM `genome_blog_feed` WHERE `version` < ?",
-          {}, $version );
+        $item->{blog}    = $name;
+        $item->{version} = $version;
+        $self->_update_item($item);
       }
     }
   );
