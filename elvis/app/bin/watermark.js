900c200c500a2f27fb8d57f282f1f08e555bfc3a
Andy Armstrong <andy@hexten.net>
2016-11-23 21:20:45 +0000

    Run jobs sequentially.

--- elvis/app/bin/watermark.js
+++ elvis/app/bin/watermark.js
@@ -31,8 +31,6 @@ let config = Object.assign({
   output: "watermarked"
 }, opt.parsedOption.options);
 
-console.log(config);
-
 if (config.watermark === undefined)
   throw new Error("--watermark is a required option");
 
@@ -40,48 +38,58 @@ const df = promisify(dir.files);
 
 let wm = loadImage(config.watermark).catch(function(err) {
   console.log("Can't load " + config.watermark + ": ", err);
+}).then(function(iwm) {
+  for (let arg of opt.parsedOption.argv) {
+    df(arg).then(function(files) {
+      let prev = sleep(1);
+      for (let file of files) {
+        let ext = path.extname(file).toLowerCase();
+        if (okExtension.indexOf(ext) < 0) continue;
+        let out = path.join(config.output, path.relative(arg, file));
+
+        prev = prev.then(function() {
+          return loadImage(file).then(function(img) {
+            console.log("Watermarking " + file + " as " + out);
+            let cvs = new Canvas(img.width, img.height);
+            let ctx = cvs.getContext("2d");
+
+            let width = parseFloat(config.width) / 100;
+            let height = parseFloat(config.height) / 100;
+            let hpos = parseFloat(config.hpos) / 100;
+            let vpos = parseFloat(config.vpos) / 100;
+
+            let maxw = img.width * width;
+            let maxh = img.height * height;
+
+            let scale = Math.min(maxw / iwm.width, maxh / iwm.height);
+            let ow = Math.round(iwm.width * scale);
+            let oh = Math.round(iwm.height * scale);
+            let ox = Math.round((img.width - ow) * hpos);
+            let oy = Math.round((img.height - oh) * vpos);
+
+            ctx.drawImage(img, 0, 0);
+            ctx.drawImage(iwm, ox, oy, ow, oh);
+
+            return mkdirp(path.dirname(out)).then(function() {
+              return saveImage(out, cvs);
+            }).catch(function(err) {
+              console.log("Error: ", err);
+              throw new Error(err);
+            });
+
+          }).catch(function(err) {
+            console.log(err)
+          });
+        });
+      }
+    });
+  }
+
 });
 
-for (let arg of opt.parsedOption.argv) {
-  df(arg).then(function(files) {
-    for (let file of files) {
-      let ext = path.extname(file).toLowerCase();
-      if (okExtension.indexOf(ext) < 0) continue;
-      let out = path.join(config.output, path.relative(arg, file));
-
-      RSVP.all([wm, loadImage(file), mkdirp(path.dirname(out))]).then(function(images) {
-        let iwm = images[0];
-        let img = images[1];
-        console.log("Watermarking " + file + " as " + out);
-        let cvs = new Canvas(img.width, img.height);
-        let ctx = cvs.getContext("2d");
-
-        let width = parseFloat(config.width) / 100;
-        let height = parseFloat(config.height) / 100;
-        let hpos = parseFloat(config.hpos) / 100;
-        let vpos = parseFloat(config.vpos) / 100;
-
-        let maxw = img.width * width;
-        let maxh = img.height * height;
-
-        let scale = Math.min(maxw / iwm.width, maxh / iwm.height);
-        let ow = Math.round(iwm.width * scale);
-        let oh = Math.round(iwm.height * scale);
-        let ox = Math.round((img.width - ow) * hpos);
-        let oy = Math.round((img.height - oh) * vpos);
-
-        ctx.drawImage(img, 0, 0);
-        ctx.drawImage(iwm, ox, oy, ow, oh);
-
-        saveImage(out, cvs).catch(function(err) {
-          console.log("Error: ", err);
-          throw new Error(err);
-        });
-
-      }).catch(function(err) {
-        console.log(err)
-      });
-    }
+function sleep(n) {
+  return new RSVP.Promise(function(resolve, reject) {
+    setTimeout(resolve, n);
   });
 }
 
