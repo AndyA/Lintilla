4c2ca3fa63071ee255833bf7fe3477df7e5fa8a3
Andy Armstrong <andy@hexten.net>
2017-01-05 15:17:08 +0000

    Standardise URI handling.

--- Util/Dancer.pm
+++ Util/Dancer.pm
@@ -4,10 +4,12 @@ use strict;
 use warnings;
 
 use Dancer ':syntax';
+use URI;
+use URI::QueryParam;
 
 use base qw( Exporter );
 
-our @EXPORT_OK = qw( our_uri_for );
+our @EXPORT_OK = qw( our_uri_for cache_bust );
 our %EXPORT_TAGS = ( all => [@EXPORT_OK] );
 
 =head1 NAME
@@ -16,10 +18,29 @@ Lintilla::Util::Dancer - Dancer related utilities
 
 =cut
 
-sub our_uri_for {
+sub _our_uri_for {
   my $sn = delete request->env->{SCRIPT_NAME};
   my $uri = request->uri_for( join '/', '', @_ );
   request->env->{SCRIPT_NAME} = $sn;
+  return $uri;
+}
+
+sub our_uri_for {
+  my $uri = _our_uri_for(@_);
+
+  # public_uri override
+  my $public_uri = config->{public_uri};
+  return $uri unless defined $public_uri;
+
+  my $pu = URI->new($public_uri);
+  $pu->path_query( $uri->path_query );
+
+  return $pu;
+}
+
+sub cache_bust {
+  my $uri = URI->new( $_[0] );
+  $uri->query_param_append( cache_bust => rand );
   return $uri;
 }
 
